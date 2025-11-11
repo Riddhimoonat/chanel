2e8f1a84f5e49355442213f5968174ef25265b20
Riddhimoonat <riddhimoonat04@gmail.com>
2025-11-11 23:30:35 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex-center h=[100vh' >
+      <div className='text-6xl text-indigo-300 '>hey theree </div>
+    </div>
+  )
+}
+
+export default App
